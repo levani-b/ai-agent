6501ee92bb74280c99fe7753f2978dbc639fb344
levani-b <levanbirchadze@gmail.com>
2025-06-25 17:00:21 +0400

    Add CLI argument support for prompt input

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+
+if len(sys.argv) < 2 :
+    print("Usage: python script.py <unused_arg> <prompt>")
+    sys.exit(1)
+
+content = sys.argv[1]
 
 load_dotenv()
 api_key = os.environ.get('GEMINI_API_KEY')
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    contents=content)
 
 print(response.text)
 
