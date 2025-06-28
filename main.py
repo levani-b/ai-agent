e18f2edb0c91923a5abdf2f6d12ce8c5b4b3c594
levani-b <levanbirchadze@gmail.com>
2025-06-28 13:02:33 +0400

    Refactor script with verbose flag and error handling, change function structure

--- main.py
+++ main.py
@@ -2,21 +2,62 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-if len(sys.argv) < 2 :
-    print("Usage: python script.py <unused_arg> <prompt>")
-    sys.exit(1)
 
-content = sys.argv[1]
+def main():
+    if len(sys.argv) < 2 :
+        print("Usage: python3 script.py <prompt> [--verbose]")
+        sys.exit(1)
 
-load_dotenv()
-api_key = os.environ.get('GEMINI_API_KEY')
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents=content)
+    user_prompt = sys.argv[1]
 
-print(response.text)
+    verbose = False
+    if len(sys.argv) > 2 and sys.argv[2] == '--verbose':
+        verbose = True
+    elif '--verbose' in sys.argv:
+        verbose = True
+    
+    if verbose:
+        print(f'User prompt: {user_prompt}')
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    load_dotenv()
+    api_key = os.environ.get('GEMINI_API_KEY')
+
+    if not api_key:
+        print("Error: GEMINI_API_KEY not found in environment variables")
+        sys.exit(1)
+
+
+    try:
+        client = genai.Client(api_key=api_key)
+
+        messages = [
+            types.Content(role='user', parts=[types.Part(text = user_prompt)])
+        ]
+        
+        generate_content(client, messages, verbose)
+    except Exception as e:
+        print(f"Error initializing client or generating content: {e}")
+        sys.exit(1)
+    
+
+def generate_content(client, messages, verbose):
+    try:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001',
+            contents=messages)
+        
+        if verbose:
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        print(f"Response:\n{response.text}")
+    except Exception as e:
+        print(f"Error generating content: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
