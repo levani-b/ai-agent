7e233a9701dae4cb790621baff5cb864f6a8202a
levani-b <levanbirchadze@gmail.com>
2025-09-19 11:18:19 +0400

    add system prompt and pass via model config system instruction

--- main.py
+++ main.py
@@ -44,14 +44,19 @@ def main():
     
 
 def generate_content(client, messages, verbose):
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
     try:
         response = client.models.generate_content(
             model='gemini-2.0-flash-001',
-            contents=messages)
+            contents=messages,
+            config=types.GenerateContentConfig(system_instruction=system_prompt)
+            )
         
         if verbose:
                 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}",
+                      )
         
         print(f"Response:\n{response.text}")
     except Exception as e:
